e31cca7d6d340a211ac582adff8f6ec3769e4071
5712labs <5712labs>
2020-10-28 08:12:41 +0900

    201027

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Food({ name, picture }) {
+  // {fav} = props.fav
+// console.log(props)
+// return <h1>I like {fav}</h1>;
+return (
+  <div>
+    <h1>I like { name }</h1>
+    <image src={picture}></image>
+  </div>
+);
+}
+const foodIlike = [
+{
+  name: 'Kimchi',
+  image: 'https://i.ytimg.com/vi/DfhkYNgnYYg/maxresdefault.jpg'
+},
+{
+  name: 'Ramen',
+  image: 'http://cfile221.uf.daum.net/image/999083345CA821C81A74E7'
+},
+{
+  name: 'Bibimbap',
+  image: 'http://cfile26.uf.tistory.com/image/234670415552F3990D533F'
+},
+{
+  name: 'Doncasu',
+  image: 'https://i.ytimg.com/vi/2cAbT8QEUY4/maxresdefault.jpg'
+},
+];
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      {foodIlike.map(dish => (<Food name={dish.name} picture={dish.image}></Food>))}
+      {/* <Food fav="ramen"></Food>
+      <Food fav="samgiosal"></Food>
+      <Food fav="chukumi"></Food> */}
     </div>
   );
 }
